a46ca527104a2e0120658e54b94d014574006e9c
syedincharge <rizvisyedba@gmail.com>
2022-11-06 17:36:54 -0600

    Initial commit

---
+++ src/main/java/com/rizvi/mockito/brewery/BreweryMockitoTestingApplication.java
@@ -0,0 +1,13 @@
+package com.rizvi.mockito.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryMockitoTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryMockitoTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizvi/mockito/brewery/BreweryMockitoTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rizvi.mockito.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BreweryMockitoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
