c893eb9669041daca8017127cfaa4b04490b3aa3
syedincharge <rizvisyedba@gmail.com>
2022-11-06 23:14:03 -0600

    update BeerOrderControllerIT

--- src/test/java/com/rizvi/mockito/brewery/web/controllers/BeerOrderControllerIT.java
+++ src/test/java/com/rizvi/mockito/brewery/web/controllers/BeerOrderControllerIT.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BeerOrderControllerIT {
